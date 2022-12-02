fce568f6cafe7667936ed898dbcb5f5f18646abb
Kristoffer Haugsbakk <code@khaugsbakk.name>
2022-12-02 14:34:37 +0100

    Start AOC 2021

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// static input: &str = include_str!("???");
+
+fn main() {
+    // I just update this function for each problem that I bother to solve,
+    // setting the input file path and the function call.
+    // println!("{}", <something>);
+}
