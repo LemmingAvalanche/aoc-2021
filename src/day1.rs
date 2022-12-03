79552a5f2390b317d5ed7ae37db2818ff82744ea
Kristoffer Haugsbakk <code@khaugsbakk.name>
2022-12-03 11:03:05 +0100

    Try to improve d1p2 and `cargo fmt`
    Use a sliding window iterator.
    
    Unfortunate that I had to resort to collecting into a `Vec`. How could I
    avoid that?

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,7 @@
 pub fn solve_part1(input: &str) -> u32 {
-    let mut iter = input.lines().map(|s| s.parse::<u32>().expect("not an integer"));
+    let mut iter = input
+        .lines()
+        .map(|s| s.parse::<u32>().expect("not an integer"));
     let mut prevm = iter.next().unwrap();
     let mut counter: u32 = 0;
     for num in iter {
@@ -12,7 +14,9 @@ pub fn solve_part1(input: &str) -> u32 {
 }
 
 pub fn solve_part2(input: &str) -> u32 {
-    let mut iter = input.lines().map(|s| s.parse::<u32>().expect("not an integer"));
+    let mut iter = input
+        .lines()
+        .map(|s| s.parse::<u32>().expect("not an integer"));
     let mut first = iter.next().unwrap();
     let mut second = iter.next().unwrap();
     let mut third = iter.next().unwrap();
@@ -23,6 +27,26 @@ pub fn solve_part2(input: &str) -> u32 {
         second = third;
         third = num;
         let new_sum = first + second + third;
+        if new_sum > sum {
+            counter = counter + 1;
+        }
+        sum = new_sum;
+    }
+    counter
+}
+
+pub fn solve_part2_alt1(input: &str) -> u32 {
+    // Create temporary to avoid  E0716
+    let binding = input
+        .lines()
+        .map(|s| s.parse::<u32>().expect("not an integer"))
+        .collect::<Vec<u32>>();
+    let mut iter = binding.windows(3);
+    let mut first = iter.next().unwrap();
+    let mut sum: u32 = first.iter().sum();
+    let mut counter: u32 = 0;
+    for window in iter {
+        let new_sum: u32 = window.iter().sum();
         if new_sum > sum {
             counter = counter + 1;
         }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-static input: &str = include_str!("/home/kristoffer/programming/advent-of-code-2021/input-text.txt");
+static input: &str =
+    include_str!("/home/kristoffer/programming/advent-of-code-2021/input-text.txt");
 
 fn main() {
     // I just update this function for each problem that I bother to solve,
     // setting the input file path and the function call.
-    println!("{}", advent_of_code_2021::day1::solve_part2(input));
+    println!("{}", advent_of_code_2021::day1::solve_part2_alt1(input));
 }
