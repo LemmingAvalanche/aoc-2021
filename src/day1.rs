955a3276b22a6c8cdde5b214f5a2d92763407d42
Kristoffer Haugsbakk <code@khaugsbakk.name>
2022-12-03 10:32:36 +0100

    d1p2

--- src/day1.rs
+++ src/day1.rs
@@ -10,3 +10,23 @@ pub fn solve_part1(input: &str) -> u32 {
     }
     counter
 }
+
+pub fn solve_part2(input: &str) -> u32 {
+    let mut iter = input.lines().map(|s| s.parse::<u32>().expect("not an integer"));
+    let mut first = iter.next().unwrap();
+    let mut second = iter.next().unwrap();
+    let mut third = iter.next().unwrap();
+    let mut sum = first + second + third;
+    let mut counter: u32 = 0;
+    for num in iter {
+        first = second;
+        second = third;
+        third = num;
+        let new_sum = first + second + third;
+        if new_sum > sum {
+            counter = counter + 1;
+        }
+        sum = new_sum;
+    }
+    counter
+}
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ static input: &str = include_str!("/home/kristoffer/programming/advent-of-code-2
 fn main() {
     // I just update this function for each problem that I bother to solve,
     // setting the input file path and the function call.
-    println!("{}", advent_of_code_2021::day1::solve_part1(input));
+    println!("{}", advent_of_code_2021::day1::solve_part2(input));
 }
