9c1ef44c05d96e6f92ee4fc35eb4f640d7096319
Kristoffer Haugsbakk <code@khaugsbakk.name>
2022-12-03 17:05:07 +0100

    d3p2
    It ain’t pretty but it’s honest work?
    
    Also update `.gitignore`.

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,12 @@
+use Freq::*;
+
+#[derive(PartialEq, Debug)]
+enum Freq {
+    Zeros,
+    Ones,
+    Equal,
+}
+
 pub fn solve_part1(input: &str) -> u32 {
     let max_index = input.lines().nth(0).unwrap().len() - 1;
     let count_numbers = input.lines().count();
@@ -8,6 +17,53 @@ pub fn solve_part1(input: &str) -> u32 {
     let γ = build_gamma(&vector, max_index, count_numbers);
     let ε = build_epsilon(&vector, max_index, count_numbers);
     γ * ε
+}
+
+pub fn solve_part2(input: &str) -> u32 {
+    let max_index = input.lines().nth(0).unwrap().len() - 1;
+    let count_numbers = input.lines().count();
+    let vector = input
+        .lines()
+        .map(|s| u16::from_str_radix(s, 2).unwrap())
+        .collect::<Vec<u16>>();
+    let ox = ox_rating(&vector, max_index, count_numbers);
+    let co2 = co2_rating(&vector, max_index, count_numbers);
+    ox * co2
+}
+
+fn ox_rating(input: &Vec<u16>, max_index: usize, count_numbers: usize) -> u32 {
+    let mut remaining = input.clone();
+    for i in 0..=max_index {
+        if remaining.len() == 1 {
+            break;
+        }
+        let (freq, hits) = frequency(&remaining, i, max_index, remaining.len());
+        if freq == Ones || freq == Equal {
+            // NOTE duplicate code (e.g.)
+            remaining.retain(|num| ((*num >> (max_index - i)) % 2) == 1);
+        } else {
+            remaining.retain(|num| ((*num >> (max_index - i)) % 2) == 0);
+        }
+    }
+    assert!(remaining.len() == 1);
+    remaining[0] as u32
+}
+
+fn co2_rating(input: &Vec<u16>, max_index: usize, count_numbers: usize) -> u32 {
+    let mut remaining = input.clone();
+    for i in 0..=max_index {
+        if remaining.len() == 1 {
+            break;
+        }
+        let (freq, hits) = frequency(&remaining, i, max_index, remaining.len());
+        if freq == Zeros {
+            remaining.retain(|num| ((*num >> (max_index - i)) % 2) == 1);
+        } else {
+            remaining.retain(|num| !(((*num >> (max_index - i)) % 2) == 1));
+        }
+    }
+    assert!(remaining.len() == 1);
+    remaining[0] as u32
 }
 
 fn build_gamma(input: &Vec<u16>, max_index: usize, count_numbers: usize) -> u32 {
@@ -43,4 +99,28 @@ fn mostly_1s(input: &Vec<u16>, position: usize, max_index: usize, count_numbers:
         .filter(|num| ((*num >> (max_index - position)) % 2) == 1)
         .count();
     count1s > (count_numbers / 2)
+}
+
+fn frequency(
+    input: &[u16],
+    position: usize,
+    max_index: usize,
+    count_numbers: usize,
+) -> (Freq, Vec<bool>) {
+    let ones = input
+        .iter()
+        .map(|num| ((*num >> (max_index - position)) % 2) == 1)
+        .collect::<Vec<bool>>();
+    let count1s = ones.iter().filter(|t| **t).count();
+    let compare = count_numbers / 2;
+    let freq = if count1s == compare && (count_numbers % 2 == 0) {
+        Equal
+    } else if count1s == compare && (count_numbers % 2 == 1) {
+        Zeros
+    } else if count1s > compare {
+        Ones
+    } else {
+        Zeros
+    };
+    (freq, ones)
 }
